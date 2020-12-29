765497bf9850846cf93bbaa0a68d6e4f1c2febda
Bruno César <>
2020-12-29 11:57:49 -0300

    Alteração de página 404

--- src/views/pages/404.php
+++ src/views/pages/404.php
@@ -1,1 +1,8 @@
-<h1>Página não encontrada</h1>
+<?php $render('header', ['title'=>'easy password - Página não encontrada!']); ?> 
+
+<div style="text-align: center; margin: 7%;">
+    <h2>Página não encontrada!</h2><br>
+    <img style="width: 100%;" src="<?php echo BASE_ASS; ?>/assets/img/erro-404.png" alt="Página não encontrada!">
+</div>
+
+<?php $render('footer'); ?>
