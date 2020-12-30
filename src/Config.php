1c2c41b79fa2089651784716dccc5cbcc4916205
Bruno Cesar <bcs018@hotmail.com>
2020-12-29 21:16:20 -0300

    Alteração para deploy

--- src/Config.php
+++ src/Config.php
@@ -6,23 +6,19 @@ if(Environment::ENVIRONMENT == 'local'){
     define('BASE_URI', 'http://localhost/easy-password/public');
     define('BASE_ASS', 'http://localhost/easy-password');
 }else{
-    define('BASE_URI', '');
-    define('BASE_ASS', '');
+    define('BASE_URI', 'http://easypassword.ml/home');
+    define('BASE_ASS', 'http://easypassword.ml');
 }
 
 class Config {
-    const BASE_DIR = '/easy-password';
+    const BASE_DIR = 'http://easypassword.ml';
 
     const DB_DRIVER   = 'mysql';
-    const DB_HOST     = 'localhost';
-    const DB_DATABASE = 'easy-password';
-    CONST DB_USER     = 'root';
-    const DB_PASS     = '';
+    const DB_HOST     = 'sql203.epizy.com';
+    const DB_DATABASE = 'epiz_27575006_easypassword';
+    CONST DB_USER     = 'epiz_27575006';
+    const DB_PASS     = 'xs9JZAFkix8s';
 
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION   = 'index';
 }
-
-
-
-
